eaa70da81c105a1b84a2cd11bf4b15b57e883a34
Rupamthakre <rupamthakre15@gmail.com>
2023-06-15 22:17:12 +0530

    first commit

---
+++ src/app/models/loginResponse.ts
@@ -0,0 +1,16 @@
+export class LoginResponse {
+    email: string;
+    location: string;
+    message: string;
+    mobile: number;
+    success: boolean;
+    token: string;
+    uid: string;
+    userName: string;
+  
+      constructor( values: Object = {}){
+          Object.assign(this, values);
+      }
+  }
+  
+  
